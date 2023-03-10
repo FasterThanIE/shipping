9d9676d32a90d9db675c2091759eb67d163d7510
Toma <tomislav.nikolic@quantox.com>
2023-03-10 22:58:13 +0100

    * Truck brands and models

--- app/Models/TruckModels.php
+++ app/Models/TruckModels.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TruckModels extends Model
 {
     const TABLE = 'truck_models';
-
-    protected $table = self::TABLE;
-
     const TYPE_TRUCK_EURO = 0;
     const TYPE_TRUCK_AMERICAN = 1;
+    protected $table = self::TABLE;
 }
