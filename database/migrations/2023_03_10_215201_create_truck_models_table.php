9b9c9977300884fced86968a4ddd1ee9a19fc148
Toma <tomislav.nikolic@quantox.com>
2023-03-10 22:58:36 +0100

    * Models table

--- database/migrations/2023_03_10_215201_create_truck_models_table.php
+++ database/migrations/2023_03_10_215201_create_truck_models_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('truck_models', function (Blueprint $table) {
+        Schema::create(TruckModels::TABLE, function (Blueprint $table) {
 
             $table->id();
             $table->string('name')->unique();
