075217e553a69d60bd4f2e404cbfc4dfb87345a9
Toma <tomislav.nikolic@quantox.com>
2023-03-10 22:27:53 +0100

    * States migration

---
+++ app/Models/States.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class States extends Model
+{
+    const TABLE = "states";
+
+    protected $fillable = [
+        'name', 'country'
+    ];
+}
